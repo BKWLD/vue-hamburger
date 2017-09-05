337e53d2995e19d2e726c4ccaafc2dae23db446b
Robert Reinhard <robert.reinhard@bkwld.com>
2017-09-05 13:23:00 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue-hamburger
-A hamburger icon that transitions to a close icon
+# Vue Hamburger
+
+A hamburger icon that transitions to a close icon.
+
+![](http://yo.bkwld.com/303I050j2H18/Screen%20Recording%202017-09-05%20at%2001.19%20PM.gif)
+
+## Usage
+
+```javascript
+import 'vue-hamburger/index.css'
+VueHamburger = import 'vue-hamburger'
+Vue.component('hamburger', VueHamburger)
+```
+
+```html
+<header>
+	<hamburger
+		:stroke='2'
+		:gap='5'
+		color='#192a35'
+		:open.sync='open'>
+	</hamburger>
+</header>
+```
---
+++ index.vue
@@ -0,0 +1,125 @@
+<!--
+A hamburger icon that animates into an close x when open
+-->
+
+<template lang='pug'>
+
+.hamburger-button(
+	:style='buttonStyle'
+	:class='buttonClasses'
+	@click='onClick')
+	.line(
+		v-for='i in 3'
+		:style='lineStyle(i)')
+
+</template>
+
+<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<script lang='coffee'>
+
+# Add 'px' if value is a number
+unit = (val) -> if typeof val == 'number' then "#{val}px" else val
+
+# Module def
+module.exports =
+
+	props:
+
+		width: # How wide to make the hamburger
+			type: Number
+			default: 24
+
+		stroke: # How thick are the lines
+			type: Number
+			default: 2
+
+		gap: # Space between lines
+			type: Number
+			default: 4
+
+		pad: # Internal padding to make the click area bigger
+			type: Number
+			default: 10
+
+		color: # The color of the lines
+			type: String
+			default: '#000000'
+
+		open: # The open state
+			type: Boolean
+			default: false
+
+	# Store the internal open state
+	data: -> state: open: @open
+
+	computed:
+
+		# Styles on the button
+		buttonStyle: ->
+			width: unit @width + @pad * 2
+			padding: unit @pad
+		buttonClasses: -> open: @state.open
+
+		# Calculate how much to shift the lines when open
+		top: -> if @state.open then @gap + @stroke else 0
+
+	methods:
+
+		# Make the line styles
+		lineStyle: (index) ->
+			background: @color
+			height: unit @stroke
+			marginTop: unit @gap if index > 1
+			top: switch index
+				when 1 then unit @top
+				when 3 then unit @top * -1
+
+		# Handle clicks
+		onClick: ->
+			@state.open = !@state.open
+			@$emit 'update:open', @state.open
+
+</script>
+
+<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<style lang='stylus'>
+
+// Add Bukwild functions
+@import '~bukwild-stylus-library/index.styl'
+
+.hamburger-button
+	button()
+	position relative
+
+	// Add an on-down effect
+	transition transform .6s ease-out-quart
+	&:active
+		transition-duration .3s
+		transform scale(1.1)
+
+	// The individual lines
+	.line
+		width 100%
+
+		// Setup transition
+		gpu-cache()
+		position relative
+		transition background, transform, top
+		transition-duration .6s
+		transition-timing-function ease-out-quart
+
+	// Make X aout of lines
+	&.open .line
+		shift = 6px
+		background white
+		&:nth-child(1)
+			transform rotate(45deg)
+		&:nth-child(2)
+			transform scale(0.5)
+			opacity 0
+		&:nth-child(3)
+			transform rotate(-45deg)
+
+</style>
